16a4c6fed691c3e384b76b467e510e1235381a2a
cheonyongjun <yes1312@naver.com>
2025-08-27 19:05:11 +0900

    캐릭터 이동 구현

--- Assets/Scripts/Content/PlayerMove.cs
+++ Assets/Scripts/Content/PlayerMove.cs
@@ -6,10 +6,14 @@ public class PlayerMove : MonoBehaviour
 	public float speed;
 	private Rigidbody2D rb;
 	private Vector2 moveVelocity;
+	Animator anim;
+	SpriteRenderer spriter;
 
 	private void Start()
 	{
 		rb = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+		spriter = GetComponent<SpriteRenderer>();
 	}
 
 	private void Update()
@@ -22,4 +26,47 @@ public class PlayerMove : MonoBehaviour
 	{
 		rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
 	}
+
+	private void LateUpdate()
+	{
+		anim.SetFloat("Speed", Mathf.Abs(moveVelocity.x));
+
+		if (moveVelocity.x != 0)
+		{
+			spriter.flipX = moveVelocity.x < 0;
+			anim.SetTrigger("Run");
+		}
+		else
+		{
+			anim.SetTrigger("Idle");
+		}
+	}
+
+	//[SerializeField]
+	//float speed;
+	//float inputValue;
+
+	//Rigidbody2D body;
+	//Animator anim;
+	//SpriteRenderer spriter;
+
+	//private void Awake()
+	//{
+	//	body = GetComponent<Rigidbody2D>();
+	//	anim = GetComponent<Animator>();
+	//	spriter = GetComponent<SpriteRenderer>();
+	//}
+
+	//private void FixedUpdate()
+	//{
+	//	body.linearVelocityX = inputValue * speed;
+	//}
+
+
+
+	//private void OnMove(InputValue value)
+	//{
+	//	Debug.Log("Move");
+	//	inputValue = value.Get<Vector2>().x;
+	//}
 }
